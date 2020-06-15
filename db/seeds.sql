1113246eedc8df95d36caa25200b0cc0d7c60bfc
Michael Cervantes <mac95860@hotmail.com>
2020-06-14 22:50:55 -0500

    quick Seeds update

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO burgers VALUES ("Baconator", true);
-INSERT INTO burgers VALUES ("Bison Burger", true);
-INSERT INTO burgers VALUES ("Baconator", true);
+INSERT INTO burgers VALUES ("Turkey Burger", true);
+INSERT INTO burgers VALUES ("BBQ Burger", true);
 INSERT INTO burgers VALUES ("Bison Burger", true);
 INSERT INTO burgers VALUES ("Goober Burger");
