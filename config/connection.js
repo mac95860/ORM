1cc62db2a6b000b0617971b81b2762c7d1cf339c
Michael Cervantes <mac95860@hotmail.com>
2020-06-27 09:56:49 -0500

    jaws test

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,27 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "d9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  port: 3306,
-  user: "z4ud5d7mkfh1buyj",
-  password: "evhkvmp0j4kq7azl",
-  database: "dqoma5tz6o5b14i8"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+      host: "d9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+      port: 3306,
+      user: "z4ud5d7mkfh1buyj",
+      password: "evhkvmp0j4kq7azl",
+      database: "dqoma5tz6o5b14i8"
+    })
+};
+
+// const connection = mysql.createConnection({
+//   host: "d9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   port: 3306,
+//   user: "z4ud5d7mkfh1buyj",
+//   password: "evhkvmp0j4kq7azl",
+//   database: "dqoma5tz6o5b14i8"
+// });
 
 // Make connection.
 connection.connect((err) => {
